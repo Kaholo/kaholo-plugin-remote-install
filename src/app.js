1d2f6b536bae5b16c4131bca17fdefbc8e3700eb
Roy Talyosef <roy@productionmap.com>
2018-04-02 14:50:38 +0300

    Removed simple-ssh

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const child_process = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const SSH = require('simple-ssh');
 
 const env = require('../../../core/src/environment/environment');
 
@@ -23,11 +22,7 @@ function guid() {
 }
 
 function installAgent(host, user, pass, key, serverUrl, agentPort) {
-    const config = { host, user, pass, key };
-    const ssh = new SSH(config);
-
-
-    const tmp_key_file_name = guid() + '.pem';
+    const tmp_key_file_name = guid() + '.txt';
     let keypath = path.join(__dirname, tmp_key_file_name);
     if (key) {
         fs.writeFileSync(keypath, key);
@@ -44,94 +39,46 @@ function installAgent(host, user, pass, key, serverUrl, agentPort) {
         connectionSSHString += ` ${user}`
     }
     connectionSSHString += `@${host}`;
-    child_process.execSync(connectionSSHString);
-
+    // creating the directory and installing unzip
+    child_process.execSync(`${connectionSSHString} "mkdir -p ${INSTALLATION_PATH }; sudo apt install unzip -y;"`);
+    let scpActionString = 'scp';
+    if (key) {
+        scpActionString += ` -i ${keypath}`
+    }
+    scpActionString += ' ' + INSTALLATION_PACKAGE_DIST;
+    if (pass) {
+        scpActionString += ` ${user}:${pass}`
+    } else {
+        scpActionString += ` ${user}`
+    }
+    scpActionString += `@${host}:${INSTALLATION_PATH}`;
     return new Promise((resolve, reject) => {
-        ssh.exec(`mkdir -p ${INSTALLATION_PATH}`, {
-            out: function (stdout) {
-                console.log(stdout);
-            },
-            exit: function (code, stdout, stderr) {
-                console.log(code, stderr);
-                resolve();
-            }
-        }).start();
-    }).then(() => new Promise((resolve, reject) => {
 
-        /* transfer zipfile to remote server */
-        let scpActionString = 'scp';
-        if (key) {
-            scpActionString += ` -i ${keypath}`
-        }
-        scpActionString += ' ' + INSTALLATION_PACKAGE_DIST;
-        if (pass) {
-            scpActionString += ` ${user}:${pass}`
-        } else {
-            scpActionString += ` ${user}`
-        }
-        scpActionString += `@${host}:${INSTALLATION_PATH}`;
-        console.log(scpActionString);
+        // transferring the package
         child_process.exec(scpActionString, (error, stdout, stderr) => {
             if (error) {
-                console.log('an error occured');
-                return reject();
+                console.log('an error occurred', error);
+                return reject(error);
             }
             console.log(error, stdout, stderr);
             resolve();
         });
-    })).then(() => new Promise((resolve, reject) => {
-        let installationScript = fs.readFileSync(path.join(__dirname, 'install_agent.txt')).toString();
-        installationScript = installationScript.replace(new RegExp('INSTALLATION_PATH', 'g'), INSTALLATION_PATH);
-        installationScript = installationScript.replace(new RegExp('{{SERVER_URL}}', 'g'), (serverUrl || env.server_url));
-        installationScript = installationScript.replace(new RegExp('{{PORT}}', 'g'), agentPort? `--PORT=${agentPort}` : '');
+    })
+        .then(() => new Promise((resolve, reject) => {
+                let installationScript = fs.readFileSync(path.join(__dirname, 'install_agent.txt')).toString();
+                installationScript = installationScript.replace(new RegExp('INSTALLATION_PATH', 'g'), INSTALLATION_PATH);
+                installationScript = installationScript.replace(new RegExp('{{SERVER_URL}}', 'g'), (serverUrl || env.server_url));
+                installationScript = installationScript.replace(new RegExp('{{PORT}}', 'g'), (agentPort || '8090'));
 
-        console.log(installationScript);
-        ssh
-            .exec('sudo apt-get install unzip -y', {
-                out: function (stdout) {
-                    console.log(stdout);
-                },
-                exit: function (code, stdout, stderr) {
-                    console.log(code, stdout, stderr)
-                }
+                // unzipping, creating script file on remote and running it
+                child_process.execSync(`${connectionSSHString} "unzip -o ${INSTALLATION_PATH}/installation_package.zip -d ${INSTALLATION_PATH}; echo '${installationScript}' > ${INSTALLATION_PATH}/install.sh; chmod 777 ${INSTALLATION_PATH}/install.sh; sudo ${INSTALLATION_PATH}/install.sh"`);
+                resolve();
             })
-            .exec(`unzip -o ${INSTALLATION_PATH}/installation_package.zip -d ${INSTALLATION_PATH}`, {
-                out: function (stdout) {
-                    console.log(stdout);
-                },
-                exit: function (code, stdout, stderr) {
-                    console.log('')
-                }
-            })
-            .exec(`cd ${INSTALLATION_PATH}`)
-            .exec(`echo '${installationScript}' > ${INSTALLATION_PATH}/install.sh`, {
-                exit: function (code, stdout, stderr) {
-                    console.log(code, stdout, stderr);
-                }
-            })
-            .exec(`chmod 777  ${INSTALLATION_PATH}/install.sh`, {
-                out: function (stdout) {
-                    console.log('chmod ', stdout);
-                },
-                exit: function (code, stdout, stderr) {
-                    if (stderr) { return reject(stderr) }
-                    console.log(code, stdout, stderr);
-                }
-            })
-            .exec(`sudo ${INSTALLATION_PATH}/install.sh`, {
-                out: function (stdout) {
-                    console.log('script ', stdout);
-                },
-                exit: function (code, stdout, stderr) {
-                    console.log(code, stdout, stderr);
-                    resolve();
-                }
-            })
-            .start();
-    })).then(() => new Promise((resolve, reject) => {
-        fs.unlinkSync(keypath);
-        resolve();
-    }));
+        )
+        .then(() => new Promise((resolve, reject) => {
+            fs.unlinkSync(keypath);
+            resolve();
+        }));
 
 
 }
@@ -153,14 +100,19 @@ function main(argv) {
         action.params.PASS,
         action.params.KEY,
         action.params.PM_SERVER_URL,
-        action.params.PM_AGENT_PORT).then((res) => {
-        process.exit(0);
-    }).catch(err => {
-        console.log('An error occurred', err);
-        // Uncaught Fatal Exception
-        // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-        process.exit(1); // Failure
-    });
+        action.params.PM_AGENT_PORT
+    )
+        .then((res) => {
+            console.log('Finish');
+            console.log(res);
+            process.exit(0);
+        })
+        .catch(err => {
+            console.log('An error occurred', err);
+            // Uncaught Fatal Exception
+            // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
+            process.exit(1); // Failure
+        });
 }
 
 main(process.argv);
